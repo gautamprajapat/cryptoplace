634e718f63ab8ca2239c224c4c7899b21bc89001
Gautam Prajapati <gautamprajapati052@gmail.com>
2024-06-16 09:48:08 -0700

    cryptoplace APP

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <p>Copyright @ 2024, Cryptoplace -all Right Reserved.</p>
+    </div>
+  )
+}
+
+export default Footer
